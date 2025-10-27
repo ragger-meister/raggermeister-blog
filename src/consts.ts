0df20db358350405d01101c46fbe6d60cd10634f
raggermeister <anton@raggermeister.com>
2025-10-27 19:32:00 +0100

    Limpiar sitio: eliminar referencias al blog y simplificar código
    - Actualizar package.json: cambiar nombre y versión
    - Simplificar astro.config.mjs eliminando comentarios
    - Limpiar constantes: cambiar títulos y descripciones
    - Eliminar componentes innecesarios (Header, Footer, etc.)
    - Eliminar assets y fuentes del blog
    - Simplificar CSS global
    - Vaciar README.md
    - Actualizar página principal con diseño limpio

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,2 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'En construcción';
-export const SITE_DESCRIPTION = 'Sitio web en construcción';
+export const SITE_TITLE = 'Raggermeister';
+export const SITE_DESCRIPTION = 'Sitio web oficial';
